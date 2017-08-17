4804cd0453b5371b33493d9414b83b72643406eb
Ernesto Corbellini <ecorbe@gmail.com>
2017-08-16 21:46:08 -0300

    Add set channel service.

--- dmx_driver/src/driver.py
+++ dmx_driver/src/driver.py
@@ -1,24 +1,45 @@
+#! /usr/bin/env python
+
 import serial
 import termios
 import fcntl
 import time
 import rospy
+from dmx_driver.srv import SetChannel, SetChannelResponse
+
 
 class DmxDriver(object):
+
   def __init__(self, com_port="/dev/ttyUSB0"):
+    self.rate = rospy.Rate(50)
     self.dmx_frame = [0] * 512
     self.BAUD_RATE = 250000
-    self.serial_device = serial.Serial(com_port, baudrate=self.BAUD_RATE, stopbits=serial.STOPBITS_TWO)
+    try:
+      self.serial_device = serial.Serial(com_port, baudrate=self.BAUD_RATE, stopbits=serial.STOPBITS_TWO)
+    except serial.SerialException, e:
+      rospy.logerr(str(e))
     self.TIOCSBRK = getattr(termios, 'TIOCSBRK', 0x5427)
     self.TIOCCBRK = getattr(termios, 'TIOCCBRK', 0x5428)
+    self.services = [
+      rospy.Service("set_channel", SetChannel, self.set_channel_srv_cb)
+    ]
 
   def __del__(self):
     self.serial_device.close()
 
+  def run(self):
+    while not rospy.is_shutdown():
+      self.send_frame()
+      self.rate.sleep()
+
+  def set_channel_srv_cb(self, req):
+    self.set_channel(req.channel, req.value)
+    return SetChannelResponse()
+
   def set_channel(self, channel, value):
     """
     Set a channel to a specific value.
-    :param channel: A channel value in the range [0; 511]
+    :param channel: A channel value in the range [1; 512]
     :param value: A value in the range [0; 255]
     :return: None
     """
@@ -26,7 +47,7 @@ class DmxDriver(object):
     if channel > 511:
       channel = 511
     elif channel < 0:
-      channel
+      channel = 0
     self.dmx_frame[channel] = value
 
   def zero_dmx_frame(self):
@@ -54,8 +75,12 @@ class DmxDriver(object):
     time.sleep(100 / 1e6)
     self.serial_device.write(serial_data)
 
+
 if __name__ == '__main__':
-  dmx = DmxDriver()
+  rospy.init_node("dmx_driver")
+  node = DmxDriver()
+  node.run()
+  """
   a = 0
   s = 1
   #for i in range(10):
@@ -76,3 +101,4 @@ if __name__ == '__main__':
     dmx.set_channel(3, a)
     dmx.send_frame()
     time.sleep(0.02)
+  """
